3ac7ae58db632a6617b75fb9fa49038947b42067
Marcel <suterma@users.noreply.github.com>
2019-01-25 00:06:17 +0100

    Update RedmineLayoutFixer.user.js
      //TODO try a button to copy a text for selection (does not work right now)

--- RedmineLayoutFixer.user.js
+++ RedmineLayoutFixer.user.js
@@ -2,8 +2,7 @@
 // @name     Redmine Issue Header Layout fixer
 // @namespace    https://github.com/suterma/
 // @description In Redmine issue tracking, issue details view, moves the issue's content title in the issue body part, next to the exisiting subjet. This allows easy copy-paste of the issue's caption, consisting of the issue title and subject at once.
-// @copyright    opensource@marcelsuter.ch, GPLv3 License
-// @author       opensource@marcelsuter.ch
+// @author       opensource@marcelsuter.ch, GPLv3 License
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @include  https://*/redmine/issues/*
 // @version  1.0
@@ -14,15 +13,42 @@
 // ---------------------------------------
 // Changelog
 // 1.0    (2019-01-24) Initial version
+// 1.01   (2019-01-24) Try to use a button for copy-paste
 // ---------------------------------------
 
 (function () {
   'use strict';
 
   // get the content header of an issue
-  var issueHeader = $('html body.controller-issues div#wrapper div#main div#content h2');
+  var issueHeader = $("html body.controller-issues div#wrapper div#main div#content h2");
 
   // insert it into the subject of the issue
-  issueHeader.insertBefore( 'html body div#main div#content div.issue div.subject' );
+  var issueSubject = $("html body div#main div#content div.issue div.subject");
+  //issueSubject.insertAdjacentElement('afterbegin', issueHeader);
+  issueSubject.prepend(issueHeader);
+  
+  //TODO try a button to copy a text for selection (does not work right now)
+  var button = document.createElement('input');
+  button.type  = 'button';
+  button.value = 'copy';
+  button.addEventListener('click', function() {
+    //$("div.subject").innerHTML.select();
+    //document.execCommand("copy");
+    //alert("Copied the text: " + $("html body div#main div#content div.issue div.subject").innerText);
+    var subjectNode = $("div.subject h3");
+    alert(subjectNode.text());
+    //TODO continue: Der subject node ist irgendwie nicht richtig definiert. Das untenstehende Select funktioniert nicht
+    
+    var range = document.createRange();
+     range.selectNode(subjectNode);
+     document.window.getSelection().addRange(range);
+     document.execCommand("copy");
+     alert("text copied") ;
+    
+    
+  }, false);
+  //button.insertBefore(issueHeader);
+  //issueHeader.appendChild(button);
+  issueSubject.append(button);
 
 }) ();
