ff5d541ba5635b7deb848584420e6f870dcf4f08
Marcel <suterma@users.noreply.github.com>
2019-02-08 07:31:00 +0100

    Update RedmineLayoutFixer.user.js

--- RedmineLayoutFixer.user.js
+++ RedmineLayoutFixer.user.js
@@ -19,24 +19,24 @@
 
 (function () {
   'use strict';
-    //Add the styles
-    function addGlobalStyle(css) {
-        var head, style;
-        head = document.getElementsByTagName('head')[0];
-        if (!head) { return; }
-        style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = css;
-        head.appendChild(style);
-    }
+  //Add the styles
+  function addGlobalStyle(css) {
+      var head, style;
+      head = document.getElementsByTagName('head')[0];
+      if (!head) { return; }
+      style = document.createElement('style');
+      style.type = 'text/css';
+      style.innerHTML = css;
+      head.appendChild(style);
+  }
 
-    //Add css (this is a minified version of the RedmineLayoutFixer.user.css file, processed by https://cssminifier.com/)
-    addGlobalStyle(".copy-icon{background-image:url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjgiIGhlaWdodD0iOCIgZGF0YS1pY29uPSJmaWxlIiB2aWV3Qm94PSIwIDAgOCA4Ij4gPHBhdGggc3R5bGU9ImZpbGw6cmdiKDg1LCA4NSwgODUpOyIgZD0iTTAgMHY4aDd2LTRoLTR2LTRoLTN6bTQgMHYzaDNsLTMtM3oiIC8+PC9zdmc+);background-size:1em;background-repeat:no-repeat;background-position:center;height:1.5em;width:1.5em;display:inline-flex;margin-left:5px}#header,#main,#top-menu,#wrapper3,div.subject h2{margin:0}.copy-icon:hover{background-image:url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjgiIGhlaWdodD0iOCIgZGF0YS1pY29uPSJmaWxlIiB2aWV3Qm94PSIwIDAgOCA4Ij4gPHBhdGggc3R5bGU9ImZpbGw6cmdiKDE5OCwgMjYsIDI2KTsiIGQ9Ik0wIDB2OGg3di00aC00di00aC0zem00IDB2M2gzbC0zLTN6IiAvPjwvc3ZnPg==);text-decoration-line:none}div.issue{background:0 0;padding:0;border:none}#content{background-color:#ffd}#issue-changesets div.changeset:nth-child(2n),.even,div#issue-changesets,table.list:not(.odd-even) tbody tr:nth-child(2n){background:#f0f0f0}");
+  //Add css (this is a minified version of the RedmineLayoutFixer.user.css file, processed by https://cssminifier.com/)
+  addGlobalStyle(".copy-icon{background-image:url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjgiIGhlaWdodD0iOCIgZGF0YS1pY29uPSJmaWxlIiB2aWV3Qm94PSIwIDAgOCA4Ij4gPHBhdGggc3R5bGU9ImZpbGw6cmdiKDg1LCA4NSwgODUpOyIgZD0iTTAgMHY4aDd2LTRoLTR2LTRoLTN6bTQgMHYzaDNsLTMtM3oiIC8+PC9zdmc+);background-size:1em;background-repeat:no-repeat;background-position:center;height:1.5em;width:1.5em;display:inline-flex;margin-left:5px}#header,#main,#top-menu,#wrapper3,div.subject h2{margin:0}.copy-icon:hover{background-image:url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjgiIGhlaWdodD0iOCIgZGF0YS1pY29uPSJmaWxlIiB2aWV3Qm94PSIwIDAgOCA4Ij4gPHBhdGggc3R5bGU9ImZpbGw6cmdiKDE5OCwgMjYsIDI2KTsiIGQ9Ik0wIDB2OGg3di00aC00di00aC0zem00IDB2M2gzbC0zLTN6IiAvPjwvc3ZnPg==);text-decoration-line:none}div.issue{background:0 0;padding:0;border:none}#content{background-color:#ffd}#issue-changesets div.changeset:nth-child(2n),.even,div#issue-changesets,table.list:not(.odd-even) tbody tr:nth-child(2n){background:#f0f0f0}");
 
   // get the content header of an issue
   var issueHeader = $("html body.controller-issues div#wrapper div#main div#content h2");
 
-  // insert it into the subject of the issue
+  // insert it into the subject of the issue, to have it inline within the issue
   var issueSubject = $("html body div#main div#content div.issue div.subject");
   issueSubject.prepend(issueHeader);
   
@@ -45,15 +45,18 @@
   anchor.setAttribute('href', 'javascript:void(0)');
   anchor.innerHTML = '&nbsp;';
   anchor.className = "copy-icon";
-  anchor.title ='Click to copy the subject to the clipboard.';
+  anchor.title ='Click to copy the subject to the clipboard, using foswiki markup.';
   anchor.addEventListener('click', function() {    
     var outputIssueHeaderText = $("html body div#main div#content div.issue div.subject h2").text();
     var outputIssueSubjectText = $("html body div#main div#content div.issue div.subject h3").text();
+    //Replace leading 'CR' to ChangeRequest, for typical use as foswiki WikiWord
+    var foswikiOutputIssueSubjectText = outputIssueSubjectText.replace(/^CR\s/gm, 'ChangeRequest');
     var subjectNode = $("div.subject");
-    var outputText = outputIssueHeaderText + " - " + outputIssueSubjectText;
+    var outputText = outputIssueHeaderText + " - " + foswikiOutputIssueSubjectText;
     navigator.clipboard.writeText(outputText);
   }, false);
   
+  //Attach the anchor link to the subject
   var subjectTitle = $("div.subject h3");
   subjectTitle.append(anchor);
 }) ();
